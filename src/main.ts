7e0277b0152e4e70da07d24a127076d71d72e7cd
Huang <596417202@qq.com>
2022-09-27 17:54:39 +0800

    wip-font-awesome-icon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue';
 import { setupStore } from '@/state';
 import '@/assets/style/main.scss';
 
+/* import fontawesome icon */
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+
 export function createApp() {
   const app = createSSRApp(App);
 
--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
   import BasicButton from '@/components/BasicButton/index.vue';
   import AppProvider from '@/components/AppProvider/inedx.vue';
+  import FontAwesomeIcon from '@/horn_ui/h-icon/font-awesome-icon/index.vue';
   import { useRouter } from '@/hooks/router';
 
   const router = useRouter();
@@ -15,6 +16,7 @@
     <view class="flex-row justify-center">
       <BasicButton @click="jumpList1">List1 → </BasicButton>
     </view>
+    <FontAwesomeIcon />
   </AppProvider>
 </template>
 
