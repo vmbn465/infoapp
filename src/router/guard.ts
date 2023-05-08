825db5362af05a56c3aecf1b8f4ac9de5542d4a7
h_mo <596417202@qq.com>
2023-05-08 21:17:04 +0800

    feat: tabBar页面登录优化

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -16,7 +16,7 @@ function createBeforeEachGuard(router: Router) {
         } else if (!authStore.isLogin && to && to.name !== 'Login') {
             // 如果没有登录且目标路由不是登录页面则跳转到登录页面
             // 将目标路由和参数传入登录页面，登录成功后直接跳转到目标路由，优化体验
-            next({ name: 'Login', params: { redirect: to.name, ...to.query }, navType: 'replaceAll' });
+            next({ name: 'Login', params: { redirect: to.name!, tabBar: to?.meta?.tabBar, ...to.query }, navType: 'push' });
         } else if (authStore.isLogin && to && to.name === 'Login') {
             // 如果已经登录且目标页面是登录页面则跳转至首页
             next({ name: 'Home', navType: 'replaceAll' });
@@ -28,10 +28,13 @@ function createBeforeEachGuard(router: Router) {
 
 function createAfterEachGuard(router: Router) {
     router.afterEach((to) => {
+        console.log('afterEach', to);
+        // @ts-ignore
+        if (to && to?.meta?.ignoreAuth) return;
         const authStore = useAuthStore();
         if (!authStore.isLogin && to && to.name !== 'Login') {
             // 如果没有登录且目标路由不是登录页面则跳转到登录页面
-            router.replaceAll({ name: 'Login' });
+            router.push({ name: 'Login', tabBar: to?.meta?.tabBar, ...to.query });
         } else if (authStore.isLogin && to && to.name === 'Login') {
             // 如果已经登录且目标页面是登录页面则跳转至首页
             router.replaceAll({ name: 'Home' });
